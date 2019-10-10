792151625784dcabd636849bd7147012b3d98d3e
berickson <blake.erickson721@gmail.com>
2019-10-09 19:13:37 -0700

    formatting

--- index.js
+++ index.js
@@ -4,31 +4,35 @@ const { prepareResponse } = require("./lib/responses");
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
-require("dotenv").config();
 // Initialize Discord Bot
+require("dotenv").config();
+
+// This event will run if the bot starts and logs in successfully.
 bot.on("ready", () => {
-  // This event will run if the bot starts and logs in successfully.
   console.log(
     `Tomato-bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels`
   );
   bot.user.setActivity("the long game");
 });
+
+// This event triggers when the bot joins a guild (server).
 bot.on("guildCreate", guild => {
-  // This event triggers when the bot joins a guild (server).
   console.log(
     `New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`
   );
   bot.user.setActivity(`Serving ${bot.guilds.size} servers`);
 });
+
+// This event triggers when the bot is removed from a guild (server).
 bot.on("guildDelete", guild => {
-  // this event triggers when the bot is removed from a guild (server).
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
   bot.user.setActivity(`Serving ${bot.guilds.size} servers`);
 });
+
+// This event triggers when a message is sent in a guild (server) the bot belongs to.
 bot.on("message", async message => {
   // Construct a response
   const response = prepareResponse(message);
-  // console.log(response);
 
   // Send the response
   if (response) {
