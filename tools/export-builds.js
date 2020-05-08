4cb0a87b95dbe103e1cacf049462d00bb5fa60cd
berickson <blake.erickson721@gmail.com>
2020-05-07 20:18:45 -0700

    Update skills and upgrades from sheets

--- tools/export-builds.js
+++ tools/export-builds.js
@@ -1,17 +1,26 @@
 const jetpack = require("fs-jetpack");
-// const builds = require("../resources/builds.json");
+const builds = require("../resources/builds.json");
 const config = require("../resources/config.json");
 const { getUpdatedUpgrades, getUpdatedSkills } = require("./wg-api");
 const { getSheetEntry } = require("./sheets-api");
-const { reduce } = require("lodash");
+const { map, reduce } = require("lodash");
 main();
 
 async function main() {
-  await updateWGUpgrades();
-  await updateWGSkills();
-  config.sheetSkills = await getSheetEntry("skills");
-  config.sheetUpgrades = await getSheetEntry("upgrades");
+  await updateWGUpgrades().catch(err => {
+    console.error(err);
+  });
+  await updateWGSkills().catch(err => {
+    console.error(err);
+  });
+  await updateSheetSkills().catch(err => {
+    console.error(err);
+  });
+  await updateSheetUpgrades().catch(err => {
+    console.error(err);
+  });
   jetpack.write(`${jetpack.cwd()}/resources/config.json`, config);
+  jetpack.write(`${jetpack.cwd()}/resources/builds.json`, builds);
 }
 
 async function updateWGUpgrades() {
@@ -36,4 +45,30 @@ async function updateWGSkills() {
     {}
   );
   config.skillMap = newSkills;
+}
+
+async function updateSheetSkills() {
+  const newSkills = await getSheetEntry("skills");
+  return map(newSkills, skills => {
+    const shipName = skills[0];
+
+    if (builds[shipName]) {
+      builds[shipName]["skills"] = skills.slice(1);
+    } else {
+      console.log(shipName);
+    }
+  });
+}
+
+async function updateSheetUpgrades() {
+  const newUpgrades = await getSheetEntry("upgrades");
+  return map(newUpgrades, upgrades => {
+    const shipName = upgrades[0];
+
+    if (builds[shipName]) {
+      builds[shipName]["upgrades"] = upgrades.slice(1);
+    } else {
+      console.log(shipName);
+    }
+  });
 }
