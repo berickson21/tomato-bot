8b657af23fa768f99f263c4fee37e3d068e4ced7
berickson <blake.erickson721@gmail.com>
2020-05-09 19:44:09 -0700

    Updated bot to work with discord.js 12

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const TOKEN =
 // This event will run if the bot starts and logs in successfully.
 bot.on("ready", () => {
   console.log(
-    `Tomato-bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels`
+    `Tomato-bot has started, with ${bot.users.cache.size} users, in ${bot.channels.cache.size} channels`
   );
   bot.user.setActivity("%help for commands");
 });
@@ -32,21 +32,37 @@ bot.on("guildDelete", guild => {
 
 // This event triggers when a message is sent in a guild (server) the bot belongs to.
 bot.on("message", async message => {
+  const checkAdmin = message.member.roles.cache.find(
+    role =>
+      role.name === "Discord Admin" ||
+      role.name === "Clan Admin" ||
+      role.name === "AdvisoryCouncil"
+  );
   // Construct a response
+  if (checkAdmin) {
+    if (message.content === "%guilds") {
+      let guilds = [...bot.guilds.cache.values()].map(guild => guild.name);
+      message.channel.send(guilds.join("\n"));
+    } else if (message.content === "%guildCount") {
+      let guildCount = bot.users.cache.size - 1;
+      message.channel.send(guildCount.toString());
+    } else if (message.content === "%users") {
+      let users = [...bot.users.cache.filter(user => user.bot === false)].map(
+        user => user[1].username
+      );
+      message.channel.send(users.join("\n"));
+    } else if (message.content === "%userCount") {
+      let usercount = bot.users.cache.size;
+      message.channel.send(usercount.toString());
+    }
+  }
   const response = await prepareResponse(message).catch(err => {
     console.error(err);
   });
   // Send the response
   if (response) {
     try {
-      response.forEach(msg => {
-        if (msg === "guilds") {
-          let guilds = [...bot.guilds.values()].map(guild => guild.name);
-          message.channel.send(guilds.join("\n"));
-        } else if (msg) {
-          message.channel.send(msg);
-        }
-      });
+      response.forEach(msg => message.channel.send(msg));
     } catch (err) {
       console.error(err);
     }
