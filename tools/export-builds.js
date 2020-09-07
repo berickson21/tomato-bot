80ae9daf31319d9106116e15f632398fc253341e
Blake Erickson <blazer721@gmai.com>
2020-09-07 20:24:09 +0000

    Write errors to file

--- tools/export-builds.js
+++ tools/export-builds.js
@@ -13,22 +13,27 @@ main();
 async function main() {
   console.log("Updating the ship catalog");
   await updateShipCatalog().catch(err => {
+    jetpack.append("../errors.txt", err);
     console.error(err);
   });
   console.log("Updating the Upgrades list...");
   await updateWGUpgrades().catch(err => {
+    jetpack.append("../errors.txt", err);
     console.error(err);
   });
   console.log("Updating the Captain Skills list...");
   await updateWGSkills().catch(err => {
+    jetpack.append("../errors.txt", err);
     console.error(err);
   });
   console.log("Pulling updated Captain Skill builds from Sheets...");
   await updateSheetSkills().catch(err => {
+    jetpack.append("../errors.txt", err);
     console.error(err);
   });
   console.log("Pulling updated Upgrade builds from Sheets...");
   await updateSheetUpgrades().catch(err => {
+    jetpack.append("../errors.txt", err);
     console.error(err);
   });
   jetpack.write(`${jetpack.cwd()}/resources/config.json`, config);
